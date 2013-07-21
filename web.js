5b2613e4ff477527f5a5629bcd9527728c256ae7
Abdullah Ozturk <ozturkabdullah@gmail.com>
2013-07-21 21:33:33 +0000

    Read content from index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var getIndexContent = function() {
+	var data = fs.readFileSync("index.html");
+	return data;
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var data = getIndexContent();
+  	response.send(data.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  	console.log("Listening on " + port);
 });
